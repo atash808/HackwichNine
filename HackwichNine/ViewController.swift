a1e704349bbb0c3ac84df0203eb660a54947a750
atash808 <atash@hawaii.edu>
2022-10-26 20:04:22 -1000

    I've completed parts 1-5 and Problem Sets #1-2

--- HackwichNine/ViewController.swift
+++ HackwichNine/ViewController.swift
@@ -2,18 +2,52 @@
 //  ViewController.swift
 //  HackwichNine
 //
-//  Created by Flynn Porter on 10/26/22.
+//  Created by Atash Imanverdi on 10/26/22.
 //
+
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var segmentedControl: UISegmentedControl!
+    
+    
+    @IBOutlet var myLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        myLabel.text = "My Segmented Control"
+        
+        segmentedControl.selectedSegmentIndex = -1
     }
-
-
-}
+    
+    @IBAction func textField(_ sender: Any) {
+    }
+    
+    @IBAction func segmentedControlPressed(_ sender: Any) {
+        
+        switch segmentedControl
+              .selectedSegmentIndex {
+        case 0:
+            myLabel.text = "First Segment has been Selected"
+        case 1:
+            myLabel.text = "Second Segment has been Selected"
+        case 2:
+            myLabel.text = "Whoohoo, this makes sense now"
+        default:
+            break
+            
+            
+        
+        }
+        }
+        
+        
+        
+        
+        
+    }
 
